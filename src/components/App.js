90cf2a75965e3eb14a25d8d4ea3d0dad25b86644
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-25 21:40:59 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
+import React, { useState, useMemo } from "react";
+import "../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const generateTasks = () => {
+  const tasks = [];
+  for (let i = 1; i <= 50; i++) {
+    tasks.push({
+      id: i,
+      title: `Todo ${i}`,
+      completed: i % 2 == 1,
+    });
+  }
+  return tasks;
+};
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [tasks] = useState(generateTasks());
+  const [filter, setFilter] = useState("all");
 
-export default App
+  const filteredTasks = useMemo(() => {
+    console.log("Filtering tasks...");
+    switch (filter) {
+      case "active":
+        return tasks.filter((task) => !task.completed);
+      case "completed":
+        return tasks.filter((task) => task.completed);
+      default:
+        return tasks;
+    }
+  }, [filter, tasks]);
+
+  const handleFilterChange = (filterType) => {
+    setFilter(filterType);
+  };
+
+
+  return (
+    <div className='app'>
+      <button onClick={() => handleFilterChange("all")}>All</button>
+      <button onClick={() => handleFilterChange("active")}>Active</button>
+      <button onClick={() => handleFilterChange("completed")}>Completed</button>
+      
+      <div className="line"></div>
+      <h3>Note: List is artificially slowed down!</h3>
+      <ul>
+        {filteredTasks.map(task=>
+          <li className={task.completed ? 'active': ''} key={task.id}>{task.title}</li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
